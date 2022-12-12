496642472b2b43c59f20d68bb8a157042e2a9e9c
GodlinessTheOnly <118218631+GodlinessTheOnly@users.noreply.github.com>
2022-12-12 12:38:39 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,28 @@
-let mySprite = sprites.create(img`
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    myDart.throwDart()
+    myDart.angle = 0
+    myDart.setTrace(true)
+})
+let myDart: Dart = null
+myDart = darts.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . 4 4 . . . 4 4 . . . 4 4 . . 
+    . . 4 5 4 . 4 5 5 4 . 4 5 4 . . 
+    . . 4 5 5 4 5 5 5 5 4 5 5 4 . . 
+    . . 4 5 5 5 5 5 5 5 5 5 5 4 . . 
+    . . 4 4 4 4 4 4 4 4 4 4 4 4 . . 
+    . . f 5 8 9 8 5 8 8 8 9 8 f . . 
+    . f 8 4 8 9 8 5 8 8 8 9 8 8 f . 
+    . f 8 4 4 4 5 8 8 8 8 8 8 8 f . 
+    . f 4 8 f f f f f f f f f 8 f . 
+    . f 4 8 f 3 3 f 8 8 8 8 8 8 f . 
+    . 4 8 8 f f f f 8 8 8 8 8 8 f . 
+    . f f 8 8 8 8 8 8 8 8 8 8 f f . 
+    . . f f f f f f f f f f f f . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `, SpriteKind.Player)
+    `, SpriteKind.Player, 10, 110)
+myDart.setTrace(true)
+myDart.controlWithArrowKeys(true)
+myDart.angle = 0
+myDart.angleRate += 10
